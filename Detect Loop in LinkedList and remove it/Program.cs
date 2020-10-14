967839600c8cb714515ff25263ca8bee07c80314
Jaydeep Shil <ED8A@eurofins.com>
2020-10-14 18:01:19 +0530

    Add project files.

---
+++ Detect Loop in LinkedList and remove it/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Detect_Loop_in_LinkedList_and_remove_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(50);
+            head.next = new ListNode(20);
+            head.next.next = new ListNode(15);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(10);
+            head.next.next.next.next.next = head.next.next;
+            DetectCycleAndRemove(head);
+            PrintList(head);
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        static void DetectCycleAndRemove(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (slow != null && fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                {
+                    ListNode temp = head;
+                    ListNode prev = fast;
+                    while (temp != fast)
+                    {
+                        prev = fast;
+                        temp = temp.next;
+                        fast = fast.next;
+                    }
+                    prev.next = null;
+                    break;
+                }
+            }
+        }
+
+        static void PrintList(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.val + " ");
+                node = node.next;
+            }
+        }
+    }
+}
